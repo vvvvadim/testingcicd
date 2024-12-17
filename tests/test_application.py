547514ca0c717bc5a99c2e2a6238de5ac82cfa85
vadim <shooherboom@gmail.com>
2024-12-17 12:16:55 +0300

    1

--- tests/test_application.py
+++ tests/test_application.py
@@ -4,9 +4,10 @@ import json
 
 # тест на создание пользователя и проверку успешного создания
 def test_create_user():
-    user = {"name": "Fred", "address": "Moscow25", "coffee_id":"1", "has_sale": "1"}
-    url = "http://localhost/add_user"
-    r = requests.post(url, data=user)
+    user = json.dumps({"name": "Fred", "address":{ "country": "Moscow25" }, "coffee_id":"1", "has_sale": "1"})
+    url = "http://localhost:8000/add_user"
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(url, headers=headers, data=user)
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
@@ -14,13 +15,14 @@ def test_create_user():
     assert r.status_code == 201
 
 def test_find_user_by_country():
-    example_user = json.dumps({"name": "Fred", "address": "Moscow25", "coffee_id":"1", "has_sale": "1"})
-    url = "http://localhost/users_by_country"
+    example_user = json.dumps({"name": "Fred", "address":{ "country": "Moscow25" }, "coffee_id":"1", "has_sale": "1"})
+    url = "http://localhost:8000/users_by_country"
     country = json.dumps({"country": "Moscow25"})
-    r = requests.get(url, country)
+    headers = {'Content-Type': 'application/json'}
+    r = requests.get(url,headers=headers ,data=country)
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print('ERROR: %s' % e)
-    user = json.loads(r.data)
-    assert example_user == user
+    user = r.json()
+    assert example_user == user[0]
