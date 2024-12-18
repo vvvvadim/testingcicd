dd5eed3c5d7909d061aa7ce26913798c04b9f368
vadim <shooherboom@gmail.com>
2024-12-18 13:56:58 +0300

    2

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
-from sqlalchemy.dialects.postgresql import JSON, ARRAY
-from sqlalchemy.orm import relationship, DeclarativeBase
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY, JSON
+from sqlalchemy.orm import DeclarativeBase, relationship
 
 
 class Base(DeclarativeBase):
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
 
+
 class Coffee(Base):
-    __tablename__ = 'coffee'
+    __tablename__ = "coffee"
 
     title = Column(String(200), nullable=False)
     origin = Column(String(200))
@@ -15,24 +16,24 @@ class Coffee(Base):
     notes = Column(ARRAY(String))
 
     def __repr__(self):
-        return(f"Coffee(id={self.id}),title={self.title}, origin={self.origin}, intensifier={self.intensifier}, notes={self.notes}")
+        return f"Coffee(id={self.id}),title={self.title}, origin={self.origin}, intensifier={self.intensifier}, notes={self.notes}"
 
     def to_json(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     name = Column(String(50), nullable=False)
     has_sale = Column(Boolean)
     address = Column(JSON)
-    coffee_id = Column(Integer, ForeignKey('coffee.id'))
+    coffee_id = Column(Integer, ForeignKey("coffee.id"))
 
     coffee = relationship("Coffee", backref="users")
 
-
     def __repr__(self):
-        return(f"User(id={self.id}),name={self.name},address={self.address}")
+        return f"User(id={self.id}),name={self.name},address={self.address}"
 
     def to_json(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
